7e658769690cf4e3922f6bdb5aee15cda6436f76
Tillboy1 <keiranm9@gmail.com>
2025-11-17 10:57:59 +0000

    adding Moddifiers

--- Assets/CsharpIntro/Scripts/CharacterBase.cs
+++ Assets/CsharpIntro/Scripts/CharacterBase.cs
@@ -22,7 +22,8 @@ public class CharacterBase : MonoBehaviour
     #region Variables
 
     public CharacterClass characterClass;
-    public int[] STATS = new int[] { 8, 10, 12, 13, 14, 16};
+    public int[] Stats = new int[] { 8, 10, 12, 13, 14, 16};
+    public int[] Moddifier = new int[] { -1, 0, 1, 1, 2, 3};
 
     public int currentHealth;
     [SerializeField] int maxHealth = 12;
@@ -36,19 +37,19 @@ public class CharacterBase : MonoBehaviour
 
         if (characterClass == CharacterClass.Barbarian)
         {
-            maxHealth = 12 + STATS[3];
+            maxHealth = 12 + Stats[3];
         }
         else if (characterClass == CharacterClass.Fighter || characterClass == CharacterClass.Paladin || characterClass == CharacterClass.Ranger)
         {
-            maxHealth = 10 + STATS[3];
+            maxHealth = 10 + Stats[3];
         }
         else if (characterClass == CharacterClass.Artificer || characterClass == CharacterClass.Bard || characterClass == CharacterClass.Cleric || characterClass == CharacterClass.Druid || characterClass == CharacterClass.Monk || characterClass == CharacterClass.Rogue || characterClass == CharacterClass.Warlock)
         {
-            maxHealth = 8 + STATS[3];
+            maxHealth = 8 + Stats[3];
         }
         else if (characterClass == CharacterClass.Sorcerer || characterClass == CharacterClass.Wizard)
         {
-            maxHealth = 6 + STATS[3];
+            maxHealth = 6 + Stats[3];
         }
 
             currentHealth = maxHealth;
