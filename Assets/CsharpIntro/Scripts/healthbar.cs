94156bb88442fc6007075e0cd3afff5422a5d8ec
Joshua cotton <62710104+death12092@users.noreply.github.com>
2025-11-17 10:29:58 +0000

    added health bar

---
+++ Assets/CsharpIntro/Scripts/healthbar.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class healthbar : MonoBehaviour
+{
+    public Scrollbar HealthBar;
+
+    [SerializeField] int Health = 10;
+    [SerializeField] int Damage = 1;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int currenthealth = Damage/Health;
+
+    }
+}
