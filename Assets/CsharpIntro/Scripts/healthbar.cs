99cb3f1d5baaf4c6b9acc624b7f22778dabd2dee
Tillboy1 <keiranm9@gmail.com>
2025-11-17 10:52:15 +0000

    commenting out error

--- Assets/CsharpIntro/Scripts/healthbar.cs
+++ Assets/CsharpIntro/Scripts/healthbar.cs
@@ -10,7 +10,7 @@ public class healthbar : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        healthbar.
+        //healthbar.
     }
 
     // Update is called once per frame
